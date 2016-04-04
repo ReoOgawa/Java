bbf0bc2ade5b15e7dd10f24b3439ad86fe4b6228
Reo Ogawa <14ec032@ms.dendai.ac.jp>
2016-04-04 15:58:54 +0900

    001
    001

---
+++ JudgeSign.java
@@ -0,0 +1,20 @@
+// “Ç‚İ‚ñ‚¾®”’l‚Ì³^•‰^‚O‚ğ”»’è
+
+import java.util.Scanner;
+
+class JudgeSign {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("®”‚ğ“ü—Í‚¹‚æF");
+		int n = stdIn.nextInt();
+
+		if (n > 0)
+			System.out.println("‚»‚ê‚Í³‚Å‚·B");
+		else if (n < 0)
+			System.out.println("‚»‚ê‚Í•‰‚Å‚·B");
+		else
+			System.out.println("‚»‚ê‚Í‚O‚Å‚·B");
+	}
+}
